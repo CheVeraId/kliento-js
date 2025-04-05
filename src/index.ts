c26ff98db8382025e8a0c08b9b9586ccfe690095
Gus Narea <gnarea@users.noreply.github.com>
2025-04-05 13:23:34 +0100

    fix lint issues

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ export { Token } from './lib/Token.js';
 export { TokenBundle } from './lib/TokenBundle.js';
 
 export type { TokenBundleOptions } from './lib/TokenBundleOptions.js';
-export type { TokenBundleVerificationOptions } from './lib/TokenBundleVerificationOptions.js';
 export type { TokenBundleVerification } from './lib/TokenBundleVerification.js';
+export type { TokenBundleVerificationOptions } from './lib/TokenBundleVerificationOptions.js';
 export type { Member, TrustAnchor } from '@relaycorp/veraid';
